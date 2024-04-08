485cea761da272ec19089ad761e3aac310367654
Sara Parra <131387591+sarap13@users.noreply.github.com>
2024-04-08 19:44:30 +0000

    FEAT About us

--- src/front/js/pages/aboutus.js
+++ src/front/js/pages/aboutus.js
@@ -11,7 +11,7 @@ import imagenThePractices from "../../img/thepracticesSINFONDO.png"
 
 export const AboutUs = () => {
 	const [state, setState] = useState({
-   		
+
 	});
 
 
@@ -19,41 +19,38 @@ export const AboutUs = () => {
 
 
 	useEffect(() => {
-		
+
 	}, []);
 
 
 
 
 	return (
-   
+
 
 		<div className="backgroundWaves container-fluid mt-5 pt-5 text-center min-vh-100">
-		<div className="d-flex justify-content-center mt-4 pt-4">   
-			<h1 className="poiret-one-regular fontaboutus">About Us</h1>
-		</div> 
-	
-		<div className="container-fluid mt-2 d-block justify-content-center">
-		<div className="container-fluid d-block justify-content-center stylebackgroundsquare p-5">
-            <img src={logo} className="ms-3" alt="..." style={{width: "50%"}}/>
-			<div className="mt-3">
-				<span className="text-white poiret-one-regular">
-					Ocean of Om is a worldwide Yoga Hub, which looks for create a yoga community that doesn't know about separation. That is unified by one unique ocean. We want to put down the walls between the sky and the sea, the students and the teachers, one country and another and between us and you. Our purpose is to create a place for you to practice with any teacher without the barriers of physical distance. Also, we want teachers to expand their spiritual teachings to the whole world. We know changing the globe can be a huge purpose, but at least we want to touch people's heart. Your heart.
-				</span>
+			<div className="mx-auto opacity-75 min-vh-100">
+				<div className="d-flex justify-content-center mt-4 pt-4 mx-auto row">
+					<h1 className="poiret-one-regular text-dark col-sm-12 col-md-6">About Us</h1>
+				</div>
+				<div className="container-fluid d-flex justify-content-center ">
+					<div className="p-5 pt-3">
+						<img className="img-fluid rounded-6" src="https://www.webconsultas.com/sites/default/files/styles/wch_image_schema/public/temas/yoga.jpg" style={{ maxWidth: "20rem" }} alt="Yoga" />
+						<div className="mt-3">
+							<p className="text-dark poiret-one-regular">
+								Ocean of Om is a worldwide Yoga Hub, which looks for create a yoga community that doesn't know about separation. That is unified by one unique ocean. We want to put down the walls between the sky and the sea, the students and the teachers, one country and another and between us and you. Our purpose is to create a place for you to practice with any teacher without the barriers of physical distance. Also, we want teachers to expand their spiritual teachings to the whole world. We know changing the globe can be a huge purpose, but at least we want to touch people's heart. Your heart.
+							</p>
+						</div>
+					</div>
+				</div>
+				<div className="mt-2 pb-0">
+					<Link to="/sessions">
+						<button className="btn btn-outline-secondary">Back to sessions</button>
+					</Link>
+				</div>
 			</div>
 		</div>
 
-			<div className="col-lg-12 col-md-12 col-sm-12 mb-4 mt-4">
-				<Link to="/sessions">
-				<button className="btn btn-outline-secondary">Back to sessions</button>
-				</Link>
-			</div>
-		</div>
-			
-		
-	</div>
 
-	
-		
 	);
- };
+};
