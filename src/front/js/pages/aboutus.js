2590b26956f3b23fd85b73fa0bfaf8aaa04502c9
Sara Parra <131387591+sarap13@users.noreply.github.com>
2024-04-09 17:24:02 +0000

    margins singup y fre trial done, about us

--- src/front/js/pages/aboutus.js
+++ src/front/js/pages/aboutus.js
@@ -28,24 +28,23 @@ export const AboutUs = () => {
 	return (
 
 
-		<div className="backgroundWaves container-fluid mt-5 pt-5 text-center min-vh-100">
-			<div className="mx-auto opacity-75 min-vh-100">
+		<div className="conchabgimg container-fluid mt-5 pt-5 text-center min-vh-100">
+			<div className="mx-auto min-vh-100">
 				<div className="d-flex justify-content-center mt-4 pt-4 mx-auto row">
-					<h1 className="poiret-one-regular text-dark col-sm-12 col-md-6">About Us</h1>
+					<h1 className="poiret-one-regular text-white fw-bold col-sm-12 col-md-6">About Us</h1>
 				</div>
-				<div className="container-fluid d-flex justify-content-center ">
+				<div className="container-fluid d-flex justify-content-center row">
 					<div className="p-5 pt-3">
-						<img className="img-fluid rounded-6" src="https://www.webconsultas.com/sites/default/files/styles/wch_image_schema/public/temas/yoga.jpg" style={{ maxWidth: "20rem" }} alt="Yoga" />
-						<div className="mt-3">
-							<p className="text-dark poiret-one-regular">
+						<div className="mt-3 p-5 bg-white opacity-50">
+							<p className="text-black poiret-one-regular fw-bold fs-5">
 								Ocean of Om is a worldwide Yoga Hub, which looks for create a yoga community that doesn't know about separation. That is unified by one unique ocean. We want to put down the walls between the sky and the sea, the students and the teachers, one country and another and between us and you. Our purpose is to create a place for you to practice with any teacher without the barriers of physical distance. Also, we want teachers to expand their spiritual teachings to the whole world. We know changing the globe can be a huge purpose, but at least we want to touch people's heart. Your heart.
 							</p>
 						</div>
 					</div>
 				</div>
-				<div className="mt-2 pb-0">
+				<div className="mt-2 pb-3">
 					<Link to="/sessions">
-						<button className="btn btn-outline-secondary">Back to sessions</button>
+						<button className="btn btn-light">Back to sessions</button>
 					</Link>
 				</div>
 			</div>
